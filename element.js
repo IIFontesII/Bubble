13b178e7f7237cb37a05c226148ccf293d8b0fe7
vinicius henrique ferreira fontes <51033680+IIFontesII@users.noreply.github.com>
2020-12-02 17:55:02 -0300

    Add files via upload

---
+++ element.js
@@ -0,0 +1,53 @@
+function dots() {
+   let right = document.getElementById('slide-cont').style.marginLeft = '0px';;
+
+   if (right == '0px') {
+       right = document.getElementById('slide-cont').style.marginLeft = '0px';
+   } 
+   
+   document.getElementById('bg').style.backgroundColor = '#ccc';
+   document.getElementById('bg1').style.backgroundColor = '#aaa';
+   document.getElementById('bg2').style.backgroundColor = '#aaa';
+   document.getElementById('bg3').style.backgroundColor = '#aaa';
+}
+
+
+function dots2() {
+    let right = document.getElementById('slide-cont').style.marginLeft = '0px';;
+ 
+    if (right == '0px') {
+        right = document.getElementById('slide-cont').style.marginLeft = '-880px';
+    } 
+    
+    document.getElementById('bg1').style.backgroundColor = '#ccc';
+    document.getElementById('bg').style.backgroundColor = '#aaa';
+    document.getElementById('bg2').style.backgroundColor = '#aaa';
+    document.getElementById('bg3').style.backgroundColor = '#aaa';
+ }
+
+
+ function dots3() {
+    let right = document.getElementById('slide-cont').style.marginLeft = '0px';;
+ 
+    if (right == '0px') {
+        right = document.getElementById('slide-cont').style.marginLeft = '-1760px';
+    } 
+    document.getElementById('bg2').style.backgroundColor = '#ccc';
+    document.getElementById('bg').style.backgroundColor = '#aaa';
+    document.getElementById('bg1').style.backgroundColor = '#aaa';
+    document.getElementById('bg3').style.backgroundColor = '#aaa';
+ }
+
+
+ function dots4() {
+    let right = document.getElementById('slide-cont').style.marginLeft = '0px';;
+ 
+    if (right == '0px') {
+        right = document.getElementById('slide-cont').style.marginLeft = '-2640px';
+    } 
+    document.getElementById('bg3').style.backgroundColor = '#ccc';
+    
+    document.getElementById('bg').style.backgroundColor = '#aaa';
+    document.getElementById('bg2').style.backgroundColor = '#aaa';
+    document.getElementById('bg1').style.backgroundColor = '#aaa';
+ }
